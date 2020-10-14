55faeddf9081dd31332642d235d19cf4a04a8b63
Aarij Rehman <aarij.rehman@gmail.com>
2020-10-14 01:59:00 -0500

    Added concurrency and FIN closing message (3a)

--- streamer.py
+++ streamer.py
@@ -3,6 +3,9 @@ from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
 import struct
+import time
+import concurrent.futures
+import sys
 
 
 class Streamer:
@@ -14,9 +17,14 @@ class Streamer:
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
+        # Parameters for managing order
         self.current_receiving_SEQ = 0
         self.current_sending_SEQ = 0
         self.buffer = {}
+        # Thread management
+        self.closed = False
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        self.listen = executor.submit(self.listener)
 
     def send(self, data_bytes: bytes) -> None:
         byte_ls = self.__byte_breaker(data_bytes, 1468)
@@ -27,27 +35,35 @@ class Streamer:
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""      
-        # If the desired packet is already in the buffer, return it
-        if self.current_receiving_SEQ in self.buffer:
-            self.current_receiving_SEQ += 1
-            return self.buffer.pop(self.current_receiving_SEQ-1)
+        # If the desired packet is already in the buffer, return it. Othwerise, wait.
+        while self.current_receiving_SEQ not in self.buffer:
+            time.sleep(0.01)
+        self.current_receiving_SEQ += 1
+        return self.buffer.pop(self.current_receiving_SEQ-1)
 
-        # Accept new data until the desired packet arrives 
-        while True:
-            data, addr = self.socket.recvfrom()
-            seq, seg = self.__unpacker(data)
-            # Return the desired packet upon receipt
-            if seq == self.current_receiving_SEQ:
-                self.current_receiving_SEQ += 1
-                return seg
-            # Add extra packets to the buffer
-            self.buffer[seq] = seg
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
         # your code goes here, especially after you add ACKs and retransmissions.
-        pass
+        # Send FIN message to let TCP know to close the connection
+        self.__send_fin()
+        self.closed = True
+        self.socket.stoprecv()
+
+    def listener(self):
+        while not self.closed:  
+            try:
+                data, addr = self.socket.recvfrom()
+                seq, seg = self.__unpacker(data)
+                # Upon receipt of FIN flag, end process
+                if seq == -1:
+                    print('closing')
+                    return
+                self.buffer[seq] = seg
+            except Exception as e:
+                print("listener died!")
+                print(e)
     
     def __byte_breaker(self, b: bytes, s: int):
         return [b[i:i+s] for i in range(0, len(b), s)]
@@ -59,3 +75,7 @@ class Streamer:
     def __unpacker(self, packed) -> tuple:
         f = f'i {len(packed)-4}s'
         return struct.unpack(f, packed)
+
+    def __send_fin(self) -> None:
+        FIN = struct.pack('i 4s', -1, b'done')
+        self.socket.sendto(FIN, (self.dst_ip, self.dst_port))

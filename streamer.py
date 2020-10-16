1c52249492274d1324ce60dfde6063e839c213e4
Aarij Rehman <aarij.rehman@gmail.com>
2020-10-16 00:24:07 -0500

    Finalized FIN handshake. this took a minute no cap

--- streamer.py
+++ streamer.py
@@ -8,8 +8,6 @@ from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
 
-
-
 class Streamer:
     def __init__(self, dst_ip, dst_port,
                  src_ip=INADDR_ANY, src_port=0):
@@ -32,7 +30,9 @@ class Streamer:
 
         # ACK management
         self.ACK = {}
-        self.FIN = False
+
+        # FIN handshake 
+        self.FIN = False # has the other party sent the fin message yet?
 
     def send(self, data_bytes: bytes) -> None:
         byte_ls = self.__byte_breaker(data_bytes, 1456)
@@ -55,9 +55,12 @@ class Streamer:
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
+        # FIN Handshake
         self.__send_fin()
         while not self.FIN:
             time.sleep(0.01)
+        # buffer in case second ACK dosn't make it
+        time.sleep(3)
         self.closed = True
         self.socket.stoprecv()
         while not self.thread.done():
@@ -103,20 +106,26 @@ class Streamer:
         check, seq, seg, ack = self.__unpacker(_data)
         if not self.__hash_check(_data):
             return 
-        if seq == -1:
+        if ack:
+            self.ACK[seq] = True   
+        elif seq == -1:
             self.FIN = True
-        elif ack:
-            self.ACK[seq] = True
+            self.__send_ACK(seq)
         else:
             self.buffer[seq] = seg
             self.__send_ACK(seq)
 
+
+    #### Helpers for sending packets
+    # Sends an ACK for a given seq number
     def __send_ACK(self, seq):
         f = 'i s b'
         s = struct.pack(f, seq, b'a', True)
         s = self.__hash_pack(s)
         self.socket.sendto(s, (self.dst_ip, self.dst_port))
 
+    # Waits until the current ACK arrives. Resends packet
+    # if it takes too long.
     def __wait_ACK(self, data):
         count = 0
         while self.current_sending_SEQ not in self.ACK:
@@ -127,20 +136,37 @@ class Streamer:
                 self.current_sending_SEQ -= 1
                 return
 
+
+    #### Helpers for the FIN handshake
+    # Sends a FIN message to indicate that we're done sending
     def __send_fin(self) -> None:
         FIN = struct.pack('i 4s b', -1, b'done', False)
+        FIN = self.__hash_pack(FIN)
         self.socket.sendto(FIN, (self.dst_ip, self.dst_port))
+        self.__wait_fin_ACK()
+
+    # Waits until an ACK for the FIN is received. If it's not recieved 
+    # soon enough, it resends the FIN.
+    def __wait_fin_ACK(self):
+        count = 0
+        while -1 not in self.ACK:
+            time.sleep(0.01)
+            count += 1
+            if count > 25:
+                self.__send_fin()
+
 
     #### Helpers for creating and verifying hashes 
     # Given a packed struct, adds a hash to it
     def __hash_pack(self, _struct):
         f = f'i {len(_struct)}s'
-        return struct.pack(f, adler32(_struct), _struct)
+        code = adler32(_struct) % 2147483647 # keeps the result a 32 bit integer
+        return struct.pack(f, code, _struct)
 
     # Given a packed struct, determines if the hash is valid
     def __hash_check(self, _data):
         expected = self.__unpacker(_data)[0]
-        actual = adler32(_data[4:])
+        actual = adler32(_data[4:]) % 2147483647 
         if expected != actual:
             print('corrupt!')
         return expected == actual

f914b26c3c496fce4f52a92a38bab2642dee94e6
Aarij Rehman <aarij.rehman@gmail.com>
2020-10-18 18:48:08 -0500

    Cleaned up/added comments

--- streamer.py
+++ streamer.py
@@ -42,29 +42,15 @@ class Streamer:
         self.all_data = b""
         self.first_time = True
 
+
+
     def send(self, data_bytes: bytes) -> None:
         if self.first_time:
-            Timer(0.01, self.wave_check, [len(data_bytes)]).start()
+            Timer(0.01, self.__data_check, [len(data_bytes)]).start()
             self.first_time = False
         self.all_data += data_bytes
-        
-
-    def wave_check(self, num):
-        print('hello')
-        if len(self.all_data) > num:
-            return Timer(0.5, self.wave_check, [len(self.all_data)]).start()
-        return self.send_data()
-
-    def send_data(self):
-        byte_ls = self.__byte_breaker(self.all_data, 1456)
-        for data in byte_ls:
-            to_send = self.__packer(self.packing_seq, data, ack=False)
-            self.sending_buffer[self.packing_seq] = to_send
-            self.__recursive_send(self.packing_seq)
-            self.packing_seq += 1
        
-    def recv(self) -> bytes:
-        """Blocks (waits) if no data is ready to be read from the connection."""      
+    def recv(self) -> bytes:    
         # If the desired packet is already in the buffer, return it. Othwerise, wait.
         while self.current_receiving_SEQ not in self.buffer:
             time.sleep(0.01)
@@ -72,14 +58,7 @@ class Streamer:
         return self.buffer.pop(self.current_receiving_SEQ-1)
 
     def close(self) -> None:
-        """Cleans up. It should block (wait) until the Streamer is done with all
-           the necessary ACKs and retransmissions"""
-        # FIN Handshake
-        self.__send_fin()
-        while not self.FIN:
-            time.sleep(0.01)
-        # buffer in case second ACK doesn't make it
-        time.sleep(2)
+        self.__FIN_handshake()
         self.closed = True
         self.socket.stoprecv()
         while not self.listen_thread.done():
@@ -117,6 +96,27 @@ class Streamer:
             return self.__recursive_send(seq)      
 
 
+    #### Nagles Algorithm Helpers (EC) 
+    # Checks if there has been more data added to sending queue. If not, sends, otherwise
+    # it starts a timer to check again in 1 ms.
+    def __data_check(self, num):
+        if len(self.all_data) > num:
+            return Timer(0.01, self.__data_check, [len(self.all_data)]).start()
+        return self.__send_data()
+
+    # Takes the whole sending queue and sends it out with __recursive send for 
+    # guaranteed delivery.
+    def __send_data(self):
+        byte_ls = self.__byte_breaker(self.all_data, 1456)
+        for data in byte_ls:
+            to_send = self.__packer(self.packing_seq, data, ack=False)
+            self.sending_buffer[self.packing_seq] = to_send
+            self.__recursive_send(self.packing_seq)
+            self.packing_seq += 1
+        self.first_time = True
+        self.all_data = b''
+
+
     #### Helpers for packing and unpacking data with structs
     # Packs a sequence number, data, ACK flag, and hash into a struct
     def __packer(self, seq, data, ack=False) -> struct:
@@ -125,11 +125,14 @@ class Streamer:
         secure_struct = self.__hash_pack(insecure_struct)
         return secure_struct
 
-    # Unpacks a struct w/ a hash, sequence number, data, and ACK. Doesn't return the hash 
+    # Unpacks a struct w/ a hash, sequence number, data, and ACK 
     def __unpacker(self, packed) -> tuple:
         f = f'i h {len(packed)-7}s b'
         return struct.unpack(f, packed)
 
+    #### Flow Control Helpers
+    # General function that deals with received packets. Sends things 
+    # where they need to go depending on the type/state of the packet
     def __packet_handler(self, _data):
         if not _data:
             return
@@ -145,8 +148,6 @@ class Streamer:
             self.buffer[seq] = seg
             self.__send_ACK(seq)
 
-
-    #### Helpers for sending packets
     # Sends an ACK for a given seq number
     def __send_ACK(self, seq):
         f = 'h s b'
@@ -156,7 +157,7 @@ class Streamer:
 
  
     #### Helpers for the FIN handshake
-    # Sends a FIN message to indicate that we're done sending
+    # Recursively sends a FIN message to indicate that we're done sending
     def __send_fin(self) -> None:
         FIN = struct.pack('h 4s b', -1, b'done', False)
         FIN = self.__hash_pack(FIN)
@@ -173,6 +174,16 @@ class Streamer:
             if count > 25:
                 self.__send_fin()
 
+    # 1) Sends a message to indicate we're done sending data
+    # 2) Waits until we receive message 
+    # 3) After receiving the FIN, we wait two seconds to make sure our FIN ACK
+    #    doesn't get lost
+    def __FIN_handshake(self):
+        self.__send_fin()
+        while not self.FIN:
+            time.sleep(0.01)
+        time.sleep(3)
+        
 
     #### Helpers for creating and verifying hashes 
     # Given a packed struct, adds a hash to it
